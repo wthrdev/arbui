be9cec56eb33dc8209bac154a494000574758e3e
with'r <107897693+wthrdev@users.noreply.github.com>
2025-07-12 22:42:21 -0300

    Revert "Cleanup"
    This reverts commit 9a5f007c6c30c1d0c1d18b7b2d7528d65b3f4106.

---
+++ src/core/resticProfile.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from dataclasses import dataclass
+
+@dataclass
+class ResticProfile:
+    repositoryPath: str = "";
+    unsafePwd: str = "";
+
+    backupPath: list[str] = [];
+
+
+    def login(self, path: Path, pwd: str):
+        pass;
+    
+    def validate(self):
+        pass;
+    
+    def logoff(self):
+        pass;
+
+    def restoreByIndex(self, index: int, overwrite: bool, basepath: str):
+        pass;
+
+    def backupByIndex(self, index: int):
+        pass;
